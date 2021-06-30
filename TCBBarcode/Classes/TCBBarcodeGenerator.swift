b8f3e6cf3bc0b954f7a7348ea1450c5a59b78801
Neil Francis Ramirez Hipona <nferocious76@gmail.com>
2021-06-30 14:07:04 +0800

    Check source string if empty before trying to generate

--- TCBBarcode/Classes/TCBBarcodeGenerator.swift
+++ TCBBarcode/Classes/TCBBarcodeGenerator.swift
@@ -174,7 +174,7 @@ public class TCBBarcodeGenerator: NSObject {
 extension TCBBarcodeGenerator {
     public func generateCode(forType type: TCBBarcodeGeneratorType, source: String) -> TCBBarcodeObject? {
         
-        guard let data = source.data(using: .utf8) else { return nil }
+        guard !source.isEmpty, let data = source.data(using: .utf8) else { return nil }
         guard let output = codeDescriptor(forType: type, data: data) else { return nil }
         
         return TCBBarcodeObject(barcode: output, transform: transform)
